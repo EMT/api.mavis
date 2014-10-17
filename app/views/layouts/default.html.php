a6111c28a29a71a1f614f8dd37aacd9826ad0c4c
Andy Gott <andy@madebyfieldwork.com>
2014-10-17 12:34:15 +0100

    Add a home page.

--- app/views/layouts/default.html.php
+++ app/views/layouts/default.html.php
@@ -10,45 +10,14 @@
 <html>
 <head>
 	<?php echo $this->html->charset();?>
-	<title>Application &gt; <?php echo $this->title(); ?></title>
-	<?php echo $this->html->style(array('bootstrap.min', 'lithified')); ?>
-	<?php echo $this->scripts(); ?>
-	<?php echo $this->styles(); ?>
+	<title>M.A.V.I.S. API</title>
 	<?php echo $this->html->link('Icon', null, array('type' => 'icon')); ?>
 </head>
 <body class="lithified">
-	<div class="container-narrow">
 
-		<div class="masthead">
-			<ul class="nav nav-pills pull-right">
-				<li>
-					<a href="http://li3.me/docs/manual/quickstart">Quickstart</a>
-				</li>
-				<li>
-					<a href="http://li3.me/docs/manual">Manual</a>
-				</li>
-				<li>
-					<a href="http://li3.me/docs/lithium">API</a>
-				</li>
-				<li>
-					<a href="http://li3.me/">More</a>
-				</li>
-			</ul>
-			<a href="http://li3.me/"><h3>&#10177;</h3></a>
-		</div>
-
-		<hr>
-
-		<div class="content">
-			<?php echo $this->content(); ?>
-		</div>
-
-		<hr>
-
-		<div class="footer">
-			<p>&copy; Union Of RAD 2013</p>
-		</div>
-
+	<div class="content">
+		<?php echo $this->content(); ?>
 	</div>
+
 </body>
 </html>
